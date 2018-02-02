e080fad8a3aec88ac05b93c5e9b6dc1dcc3bb87c
Tony Phipps <tonyphipps@gmail.com>
2018-02-02 15:32:38 +0000

    Assignment 5: Time to Practice - Conditionals & Lists

---
+++ assignment-5-time-to-practice-conditionals-and-lists/js/app.js
@@ -0,0 +1,17 @@
+new Vue({
+  el: '#exercise',
+  data: {
+    show: true,
+    array: ['Max', 'Anna', 'Chris', 'Manu'],
+    myObject: {
+      title: 'Lord of the Rings',
+      author: 'J.R.R. Tolkiens',
+      books: '3'
+    },
+    testData: {
+      name: 'TESTOBJECT',
+      id: 10,
+      data: [1.67, 1.33, 0.98, 2.21]
+    }
+  }
+});
