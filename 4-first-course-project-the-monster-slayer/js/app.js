8d50d096570cb214a2290b1a08c14bd787eb9836
Tony Phipps <tonyphipps@gmail.com>
2018-02-05 15:40:40 +0000

    Implementing a "Attack" Method

--- 4-first-course-project-the-monster-slayer/js/app.js
+++ 4-first-course-project-the-monster-slayer/js/app.js
@@ -10,6 +10,37 @@ new Vue({
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+    },
+    attack: function () {
+      var max = 10;
+      var min = 3;
+      var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.monsterHealth -= damage;
+
+      if (this.monsterHealth <= 0) {
+        alert('You won!')
+        this.gameIsRunning = false;
+        return;
+      }
+
+      max = 12;
+      min = 5
+      damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.playerHealth -= damage;
+
+      if (this.playerHealth <= 0) {
+        alert('You lost!')
+        this.gameIsRunning = false;
+      }
+    },
+    specialAttack: function () {
+
+    },
+    heal: function () {
+
+    },
+    giveUp: function () {
+
     }
   }
 })
