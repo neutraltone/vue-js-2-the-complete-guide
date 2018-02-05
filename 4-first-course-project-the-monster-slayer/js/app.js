a57c59c9af51e484647a382a4650e846dd3a8d29
Tony Phipps <tonyphipps@gmail.com>
2018-02-05 14:52:13 +0000

    Implementing a "Start Game" Method

--- 4-first-course-project-the-monster-slayer/js/app.js
+++ 4-first-course-project-the-monster-slayer/js/app.js
@@ -4,5 +4,12 @@ new Vue({
     playerHealth: 100,
     monsterHealth: 100,
     gameIsRunning: false,
+  },
+  methods: {
+    startGame: function () {
+      this.gameIsRunning = true;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+    }
   }
 })
