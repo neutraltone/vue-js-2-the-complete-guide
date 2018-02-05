05249d330d20642ef8d005a8275f6a19b48a09d5
Tony Phipps <tonyphipps@gmail.com>
2018-02-05 17:20:02 +0000

    Implementing a "Special Attack"

--- 4-first-course-project-the-monster-slayer/js/app.js
+++ 4-first-course-project-the-monster-slayer/js/app.js
@@ -16,17 +16,24 @@ new Vue({
       if (this.checkWin()) {
         return;
       }
-      this.playerHealth -= this.calculateDamage(5, 12);
-      this.checkWin();
+      this.monsterAttacks();
     },
     specialAttack: function () {
-
+      this.monsterHealth -= this.calculateDamage(10, 20);
+      if (this.checkWin()) {
+        return;
+      }
+      this.monsterAttacks();
     },
     heal: function () {
 
     },
     giveUp: function () {
 
+    },
+    monsterAttacks: function () {
+      this.playerHealth -= this.calculateDamage(5, 12);
+      this.checkWin();
     },
     calculateDamage: function (min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
