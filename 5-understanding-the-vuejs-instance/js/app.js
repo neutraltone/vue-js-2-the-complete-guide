71e8d8eba63d80a60550cc24b7f33f2f49775962
Tony Phipps <tonyphipps@gmail.com>
2018-02-06 17:28:04 +0000

    Placing $refs and Using them on your Templates

--- 5-understanding-the-vuejs-instance/js/app.js
+++ 5-understanding-the-vuejs-instance/js/app.js
@@ -10,6 +10,7 @@ var vm1 = new Vue({
     show: function () {
       this.showParagraph = true;
       this.updateTitle('The VueJS Instance (Updated)')
+      this.$refs.myButton.innerText = 'Test';
     },
     updateTitle: function (title) {
       this.title = title;
@@ -29,6 +30,7 @@ var vm1 = new Vue({
 
 vm1.newProp = 'New!';
 console.log(vm1.$data === data);
+vm1.$refs.heading.innerText = 'Something else';
 
 setTimeout(function () {
   vm1.title = 'Changed by Timer';
