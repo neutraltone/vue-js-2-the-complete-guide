b1385e9fbd21519c62a7b951e0f9df4a47bef6e2
Tony Phipps <tonyphipps@gmail.com>
2018-02-06 11:02:13 +0000

    Finishing the Action Buttons

--- 4-first-course-project-the-monster-slayer/js/app.js
+++ 4-first-course-project-the-monster-slayer/js/app.js
@@ -34,7 +34,7 @@ new Vue({
       this.monsterAttacks();
     },
     giveUp: function () {
-
+      this.gameIsRunning = false;
     },
     monsterAttacks: function () {
       this.playerHealth -= this.calculateDamage(5, 12);
