4780a68f41019d1f0e06bfd546d307cc423ac920
Tony Phipps <tonyphipps@gmail.com>
2018-02-06 17:14:31 +0000

    A Closer Look at $el and $data

--- 5-understanding-the-vuejs-instance/js/app.js
+++ 5-understanding-the-vuejs-instance/js/app.js
@@ -1,9 +1,11 @@
+var data = {
+  title: 'The VueJS Instance',
+  showParagraph: false
+}
+
 var vm1 = new Vue({
   el: '#app1',
-  data: {
-    title: 'The VueJS Instance',
-    showParagraph: false
-  },
+  data: data,
   methods: {
     show: function () {
       this.showParagraph = true;
@@ -26,7 +28,7 @@ var vm1 = new Vue({
 });
 
 vm1.newProp = 'New!';
-console.log(vm1);
+console.log(vm1.$data === data);
 
 setTimeout(function () {
   vm1.title = 'Changed by Timer';
