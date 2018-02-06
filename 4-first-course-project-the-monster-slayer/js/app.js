fc487b66afe7067642b776d0d4a437d743ff727d
Tony Phipps <tonyphipps@gmail.com>
2018-02-06 11:01:05 +0000

    Implementing a "Heal" Method

--- 4-first-course-project-the-monster-slayer/js/app.js
+++ 4-first-course-project-the-monster-slayer/js/app.js
@@ -26,7 +26,12 @@ new Vue({
       this.monsterAttacks();
     },
     heal: function () {
-
+      if (this.playerHealth <= 90) {
+        this.playerHealth += 10;
+      } else {
+        this.playerHealth = 100;
+      }
+      this.monsterAttacks();
     },
     giveUp: function () {
 
