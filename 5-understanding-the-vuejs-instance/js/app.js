de77d0afc52e8ad4c96a2137c7c341e132d8b05f
Tony Phipps <tonyphipps@gmail.com>
2018-02-06 13:06:05 +0000

    Accessing the Vue Instance from Outside

--- 5-understanding-the-vuejs-instance/js/app.js
+++ 5-understanding-the-vuejs-instance/js/app.js
@@ -1,4 +1,4 @@
-new Vue({
+var vm1 = new Vue({
   el: '#app1',
   data: {
     title: 'The VueJS Instance',
@@ -25,9 +25,18 @@ new Vue({
   }
 });
 
-new Vue({
+setTimeout(function () {
+  vm1.title = 'Changed by Timer';
+}, 3000);
+
+var vm2 = new Vue({
   el: '#app2',
   data: {
     title: 'The second Instance'
+  },
+  methods: {
+    onChange: function () {
+      vm1.title = 'Changed!';
+    }
   }
 })
