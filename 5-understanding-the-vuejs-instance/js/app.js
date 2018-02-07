6374aacf13f5eb217290f0b5cc7e86bf72c67d9c
Tony Phipps <tonyphipps@gmail.com>
2018-02-07 10:44:48 +0000

    The VueJS Instance Lifecycle in Practice

--- 5-understanding-the-vuejs-instance/js/app.js
+++ 5-understanding-the-vuejs-instance/js/app.js
@@ -1,62 +1,35 @@
-var data = {
-  title: 'The VueJS Instance',
-  showParagraph: false
-}
-
-Vue.component('hello', {
-  template: '<h1>Hello!</h1>',
-});
-
 var vm1 = new Vue({
-  data: data,
-  methods: {
-    show: function () {
-      this.showParagraph = true;
-      this.updateTitle('The VueJS Instance (Updated)')
-      this.$refs.myButton.innerText = 'Test';
-    },
-    updateTitle: function (title) {
-      this.title = title;
-    }
-  },
-  computed: {
-    lowercaseTitle: function () {
-      return this.title.toLowerCase();
-    }
-  },
-  watch: {
-    title: function (value) {
-      alert('Title changed, new value: ' + value);
-    }
-  }
-});
-
-vm1.$mount('#app1');
-
-vm1.newProp = 'New!';
-console.log(vm1.$data === data);
-vm1.$refs.heading.innerText = 'Something else';
-
-setTimeout(function () {
-  vm1.title = 'Changed by Timer';
-  vm1.show();
-}, 3000);
-
-var vm2 = new Vue({
-  el: '#app2',
+  el: '#app',
   data: {
-    title: 'The second Instance'
+    title: 'The VueJS Instance'
+  },
+  beforeCreate: function () {
+    console.log('beforeCreate()');
+  },
+  created: function () {
+    console.log('created()');
+  },
+  beforeMount: function () {
+    console.log('beforeMount()');
+  },
+  mounted: function () {
+    console.log('mounted()');
+  },
+  beforeUpdate: function () {
+    console.log('beforeUpdate()');
+  },
+  updated: function () {
+    console.log('updated()');
+  },
+  beforeDestroy: function () {
+    console.log('beforeDestroy()');
+  },
+  destroyed: function () {
+    console.log('destroyed()');
   },
   methods: {
-    onChange: function () {
-      vm1.title = 'Changed!';
+    destroy: function () {
+      this.$destroy();
     }
   }
-})
-
-var vm3 = new Vue({
-  template: '<h1>Hello!</h1>',
 });
-
-vm3.$mount();
-document.getElementById('app3').appendChild(vm3.$el);
