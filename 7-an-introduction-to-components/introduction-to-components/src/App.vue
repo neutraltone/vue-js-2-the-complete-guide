ae8440dbf700692889bb0a165de659689f473b35
Tony Phipps <tonyphipps@gmail.com>
2018-03-16 13:18:10 +0000

    The Root Component in the App.vue File

---
+++ 7-an-introduction-to-components/introduction-to-components/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>Server Status: {{ status }}</p>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      status: 'Critical'
+    };
+  }
+};
+</script>
+
+<style>
+
+</style>
