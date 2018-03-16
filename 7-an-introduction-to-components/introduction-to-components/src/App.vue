dd87c75a13b76d45b96f50b88a2cfa594e0411ad
Tony Phipps <tonyphipps@gmail.com>
2018-03-16 13:28:29 +0000

    Creating a Component

--- 7-an-introduction-to-components/introduction-to-components/src/App.vue
+++ 7-an-introduction-to-components/introduction-to-components/src/App.vue
@@ -1,15 +1,9 @@
 <template>
-  <p>Server Status: {{ status }}</p>
+  <app-server-status></app-server-status>
 </template>
 
 <script>
-export default {
-  data: function() {
-    return {
-      status: 'Critical'
-    };
-  }
-};
+
 </script>
 
 <style>
